d509f26d3251ea4b095d5ccd16d542931f82680d
Aellun <okello.k.otieono@gmail.com>
2023-09-05 16:44:03 -0700

    created executable of the files

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -4,4 +4,3 @@ for Alpha in range(97, 123):
     # Check if the letter is not 'q' or 'e' and print it without a newline
     if chr(Alpha) != 'q' and chr(Alpha) != 'e':
         print(f"{chr(Alpha)}", end='')
-
