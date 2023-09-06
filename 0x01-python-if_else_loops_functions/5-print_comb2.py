41595f99e2a5eef3ffdfeb5a7ea10d2d193bd1ec
Aellun <okello.k.otieono@gmail.com>
2023-09-06 16:51:14 -0700

    recreated the entire function of the file

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# 4-print_hexa.py
 """Print numbers from 0 to 98 in both decimal and hexadecimal."""
-for num in range(0, 99):
-    print(f"{num} = {hex(num)}")
+for num in range(0, 100):
+    if num == 99:
+        print("{:02}".format(num))
+    else:
+        print("{:02}".format(num), end=", ")
