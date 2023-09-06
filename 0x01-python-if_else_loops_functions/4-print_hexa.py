bf03d41d1afde793c6349afca1b4f858fea36874
Aellun <okello.k.otieono@gmail.com>
2023-09-06 16:43:53 -0700

    correctee th issue of two print statement in the code

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for num in range(0, 100):
-    if num == 99:
-        print("{:02}".format(num))
-    else:
-        print("{:02}".format(num), end=", ")
+"""Prints num both in decimal and hexadecimal ranging 0 to 98."""
+for num in range(0, 99):
+    print("{} = {}".format(num, hex(num)))
