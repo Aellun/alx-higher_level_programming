7fc3ef6d5d7c8940c2931d26f8b58d784746515e
Aellun <okello.k.otieono@gmail.com>
2023-09-06 17:01:10 -0700

    added the .format string format in the code to solve the error

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-# 8-uppercase.py
-
 def uppercase(str):
     """Prints a str in uppercase."""
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
             c = chr(ord(c) - 32)
-        print(f"{c}", end="")
+        print("{}".format(c), end="")
     print("")
