e5ffa2700f1980236a5e61e3940c0e00c1d039c6
Aellun <okello.k.otieono@gmail.com>
2023-09-07 18:10:26 -0700

    fixed requirement bug

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     """Print the number of arg and list of arg."""
     import sys
 
-otal = 0
+total = 0
     for n in range(len(sys.argv) - 1):
         total += int(sys.argv[n + 1])
     print("{}".format(total))
