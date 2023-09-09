3804cf8d1351dca1f64c8f46250bb64e920c3da9
Aellun <okello.k.otieono@gmail.com>
2023-09-09 02:28:02 -0700

    updated error when no arg pass output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,15 +3,15 @@
 if __name__ == "__main__":
     """Print the number of arg and list of arg."""
     import sys
-
     count = len(sys.argv) - 1
     if count == 0:
         print("Number of argument(s): 0.")
+        print(".")
     elif count == 1:
-        print("Number of argument(s): 1.")
+        print("Number of argument(s): 1:")
+        print("1:", sys.argv[1])
     else:
         print("Number of argument(s): {}.".format(count))
 
-    if count > 0:
         for n in range(count):
             print("{}: {}".format(n + 1, sys.argv[n + 1]))
