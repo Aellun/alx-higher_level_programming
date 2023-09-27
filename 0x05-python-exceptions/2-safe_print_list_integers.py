b388afba88bb232f8288fa1b184c82960467271b
Aellun <okello.k.otieono@gmail.com>
2023-09-27 03:07:46 -0700

    fixed the exception handling

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,27 +2,29 @@
 
 
 def safe_print_list_integers(my_list=None, x=0):
-
-    """Prints the first x ints from the list.
+    """Prints the first x integers from the list.
 
     Args:
         my_list (list): The list containing elements to be printed.
-        x (int): The number of ints to print.
+        x (int): The number of integers to print.
 
     Returns:
-        The number of ints printed.
+        The number of integers printed.
     """
     if my_list is None:
         my_list = []
 
     item_count = 0
     for item in my_list:
-        if item_count >= x:
-            break
+        try:
+            if item_count >= x:
+                break
 
-        if isinstance(item, int):
-            print("{:d}".format(item), end="")
-            item_count += 1
+            if isinstance(item, int):
+                print("{:d}".format(item), end="")
+                item_count += 1
+        except ValueError:
+            continue
 
     print()
     return item_count
