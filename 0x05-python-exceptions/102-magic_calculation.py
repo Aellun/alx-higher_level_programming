b975ad864c8cdd0c50e8595958a3ef84e3a8f3e7
Aellun <okello.k.otieono@gmail.com>
2023-09-27 05:14:08 -0700

    fixed loop and exception

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,13 +3,9 @@
 
 def magic_calculation(a, b):
     cal_result = 0
-    for item in range(1, 3):
+    for item in range(1, a + 1):
         try:
-            if item > a:
-                raise Exception('Too far')
-            else:
-                cal_result += a ** b / item
-        except ValueError:
-            cal_result = b + a
-            break
-    return (cal_result)
+            cal_result += a ** b / item
+        except ZeroDivisionError:
+            return "Division by zero"
+    return cal_result
