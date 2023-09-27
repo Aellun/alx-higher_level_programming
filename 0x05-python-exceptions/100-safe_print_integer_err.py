cdea756de422cbdee6d0ba6947d32ea665aaa3e1
Aellun <okello.k.otieono@gmail.com>
2023-09-27 04:27:23 -0700

    attempt to handle error exception

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 import sys
 
-
 def safe_print_integer_err(value):
-    """Prints an int with "{:d}".format()or prints an err msg to stder
+    """Prints an integer and returns True if value is an integer, else False.
+
     Args:
         value: Any value (integer, string, etc.).
 
     Returns:
-        bool: True if value is an integer and false if otherwise.
+        bool: True if value is an integer and successfully printed, False otherwise.
     """
     try:
         print("{:d}".format(int(value)))
         return True
-    except (TypeError, ValueError) as e:
+    except ValueError as e:
+        print(f"Exception: {e}", file=sys.stderr)
+        return False
+    except TypeError as e:
 
         print(f"Exception: {e}", file=sys.stderr)
         return False
