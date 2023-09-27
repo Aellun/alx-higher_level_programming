acb10d6b30d734d4961bfc975c736e3353ac703d
Aellun <okello.k.otieono@gmail.com>
2023-09-27 05:22:45 -0700

    output error fix

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 
 
+import dis
+
+
 def magic_calculation(a, b):
     cal_result = 0
     for item in range(1, a + 1):
         try:
-            cal_result += a ** b / item
+            if item > a:
+                raise Exception('Too far')
+            else:
+                cal_result += a ** b / item
         except ZeroDivisionError:
             return "Division by zero"
     return cal_result
