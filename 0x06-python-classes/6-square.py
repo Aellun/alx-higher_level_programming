232360b307ef5d4043baaee5e73e2cac0cf14bb2
Aellun <okello.k.otieono@gmail.com>
2023-09-27 18:59:53 -0700

    output error fixes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -7,18 +7,19 @@ class Square:
         Attributes: __size (int)
         Methods: __init__(self, size=0)"""
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """Initializes an instance of a new Square.
 
         Args:
             size (int): New square size.
         """
         self.size = size
+        self.position = position
 
     @property
     def size(self):
         """Get the current size of the square."""
-        return self.__size
+        return (self.__size)
 
     @size.setter
     def size(self, value):
@@ -38,18 +39,17 @@ class Square:
 
     @property
     def position(self):
-        """Getting and setting the position of the square as a tuple."""
+        """Get/set the current position of the square."""
         return (self.__position)
 
+    """getting and setting the position of the square as a tuple."""
     @position.setter
     def position(self, value):
-        """Set the position of the square.
-
-        Args:
-            value (tuple): Rep the new square pstn (2 +ve ints).
-        Raises:
-            TypeError: If 'value' is not a tuple of 2 positive ints.
-        """
+        """set the position of the square
+        Agrs:
+            value(tuple): reprep the new square(two positive ints)
+            Raises:
+                    TypeError: If 'value' is not a tuple of 2 +ve ints."""
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
@@ -58,19 +58,18 @@ class Square:
         self.__position = value
 
     def area(self):
-        """Return: int - the current area of the square."""
+        """Return: int- the current area of the square."""
         return (self.__size * self.__size)
 
     def my_print(self):
-        """Prints the square with the # char based on its pstn and size.
-        Returns: None and if size = 0 prints an empty line.
-        """
+        """Prints the square with the # character based on its pstn and size
+            returns:None and if size = 0 prints empty line"""
         if self.__size == 0:
             print("")
             return
 
-    [print("") for _ in range(0, self.__position[1])]
-    for _ in range(0, self.__size):
-        [print(" " * self.__position[0], end="")]
-        [print("#", end="") for _ in range(0, self.__size)]
-        print("")
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
