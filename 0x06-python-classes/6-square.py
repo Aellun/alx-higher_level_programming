9eb53504ae3bd7f946f5e21916a18cfe4c944627
Aellun <okello.k.otieono@gmail.com>
2023-09-27 18:41:32 -0700

    fixes output and pycodestyle error

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -69,8 +69,8 @@ class Square:
             print("")
             return
 
-        [print("") for _ in range(0, self.__position[1])]
-        for _ in range(0, self.__size):
-            [print(" ", end="") for _ in range(0, self.__position[0])]
-            [print("#", end="") for _ in range(0, self.__size)]
-            print("")
+    [print("") for _ in range(0, self.__position[1])]
+    for _ in range(0, self.__size):
+        [print(" " * self.__position[0], end="")]
+        [print("#", end="") for _ in range(0, self.__size)]
+        print("")
