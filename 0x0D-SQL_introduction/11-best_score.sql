40fb5261a7ed89225adc02d0af62db9a4003878d
Aellun <okello.k.otieono@gmail.com>
2023-11-15 07:22:39 -0800

    updated

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,6 +1,6 @@
--- Lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0
--- Displays score, name ordered by score in desc order
+-- Lists all records with a score >= 10 in the table second_table
+-- Displays score, name ordered by score in desc
 SELECT `score`, `name`
 FROM `second_table`
-WHERE 'score' >= 10;
-ORDER  BY `score` DESC
+WHERE `score` >= 10
+ORDER BY `score` DESC;
