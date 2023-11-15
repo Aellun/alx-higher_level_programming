9e6e29a972400f9c7307bf0e39c33c4b328a2733
Aellun <okello.k.otieono@gmail.com>
2023-11-15 07:02:10 -0800

    updated

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
 --list records of 'second_table' of the database hbtn_0c_0
 --display score and name in that oder(desc)
-SELECT score, name
-FROM second_table
-ORDER BY score DESC;
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
