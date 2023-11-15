2d26c94e8bb8cf276c416f5cebaee1343aaf8359
Aellun <okello.k.otieono@gmail.com>
2023-11-15 00:29:55 -0800

    correction on 0-list_databases.sql and 5-full_table.sql

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,2 +1,2 @@
 --the sql code below lists all database of my sql server
-SHOW DATABASE;
+SHOW DATABASES;
--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
--- shows the full description of the table first_table
---for hbtn_0c_0 db in my MySQL server.
-SHOW CREATE TABLE `first_table`;
+-- shows the full description of the table 'first_table'
+--for 'hbtn_0c'_0 db in my MySQL server.
+SHOW CREATE TABLE `hbtn_0c_0`.`first_table`;
