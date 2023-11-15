6a49252524f4278b7ce031125d2126fc5d7cd9d0
Aellun <okello.k.otieono@gmail.com>
2023-11-15 06:56:27 -0800

    updated

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,4 @@
 -- Creates a second_table with attributes id, name and score
---for the hbtn_0c_0 databasein my MySQL server
 CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
