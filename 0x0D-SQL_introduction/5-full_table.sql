be07b0c12c055c8f7e6a1b251f9bedb94849acfe
Aellun <okello.k.otieono@gmail.com>
2023-11-15 03:20:20 -0800

    updated

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,2 @@
--- shows the full description of the table 'first_table'
---for 'hbtn_0c'_0 db in my MySQL server.
+-- shows the full description of the table first_table
 SHOW CREATE TABLE `hbtn_0c_0`.`first_table`;
