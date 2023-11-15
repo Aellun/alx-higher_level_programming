9fdb6cafa62e925a384ad7af45571f4d811b244d
Aellun <okello.k.otieono@gmail.com>
2023-11-15 07:41:43 -0800

    updated

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,4 @@
 -- Lists all records(rows without a name value) of the table second_table
--- Displays the score and the name (in this order)
 -- Ordered in desc order based on score
 SELECT `score`, `name`
 FROM `second_table`
