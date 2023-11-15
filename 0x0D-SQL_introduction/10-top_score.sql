53b7470ccfab6f3529be371a5923c4904feb5fb9
Aellun <okello.k.otieono@gmail.com>
2023-11-15 07:12:25 -0800

    updated

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
---list records of second_table
---Order the score in descending order
+-- Lists all records of the table second_table.
+-- Records are ordered by descending score.
 SELECT `score`, `name`
 FROM `second_table`
 ORDER BY `score` DESC;
