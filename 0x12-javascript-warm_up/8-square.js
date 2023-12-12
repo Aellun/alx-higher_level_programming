b6070d5a08610422d935b0191c82d991568cc3be
Allan Kevin <134234752+Aellun@users.noreply.github.com>
2023-12-12 13:25:16 +0300

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-/*script that prints a square
+/* script that prints a square
     arg1 = size(missing size) if be converted to str)
-    char x use to print square*/
+    char x use to print square */
 
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing size');
