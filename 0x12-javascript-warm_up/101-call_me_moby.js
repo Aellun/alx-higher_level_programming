968df40c30b803904ab9a3fdf8ca029cfdb6f9ca
Allan Kevin <134234752+Aellun@users.noreply.github.com>
2023-12-12 13:03:10 +0300

    Update 101-call_me_moby.js

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-/*function that executes x times a function.
-    Prototype: function (x, theFunction)*/
+/* function that executes x times a function.
+    Prototype: function (x, theFunction) */
 exports.callMeMoby = function (x, theFunction) {
   for (let i = 0; i < x; i++) {
     theFunction();
