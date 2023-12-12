e5deced2a40016f66ff484fe57239f72227f5db3
Allan Kevin <134234752+Aellun@users.noreply.github.com>
2023-12-12 13:05:33 +0300

    Update 102-add_me_maybe.js

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-/*unction that increments and calls a function
-      Prototype: function (number, theFunction)*/
+/* unction that increments and calls a function
+      Prototype: function (number, theFunction) */
 exports.addMeMaybe = function (number, theFunction) {
   theFunction(++number);
 };
