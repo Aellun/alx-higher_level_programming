4ab4157579590e5ae71ad8269622fce978ed657c
Aellun <okello.k.otieono@gmail.com>
2023-12-13 14:16:04 -0800

    updated

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,8 +3,8 @@
     arg 1 = file path of the first source file
     arg 2 = the file path of the second source file
     arg 3 = the file path of the destination */
-const files = require('files');
+const fs = require('fs');
 
-const fArg = files.readFileSync(process.argv[2]).toString();
-const sArg = files.readFileSync(process.argv[3]).toString();
-files.writeFileSync(process.argv[4], fArg + sArg);
+const fArg = fs.readFileSync(process.argv[2]).toString();
+const sArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fArg + sArg);
