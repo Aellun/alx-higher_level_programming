fb9caeacb95425c396e33ca37402d3b8fdf11893
Aellun <okello.k.otieono@gmail.com>
2023-12-18 18:19:56 -0800

    updated

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-script that creates the State “California” 
+script that creates the State “California”
 with the City “San Francisco” from the database hbtn_0e_100_usa
 Args:
     mysql username
