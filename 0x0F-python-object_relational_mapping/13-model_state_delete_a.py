713d2f0092ba7e2723e92610e119fc4620edacc3
Aellun <okello.k.otieono@gmail.com>
2023-12-22 18:46:15 -0800

    lint correction

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" script that deletes all State objects with a name containing the letter a from the database hbtn_0e_6_usa
+""" script that deletes all State objects with a name containing
+the letter a from the database hbtn_0e_6_usa
 Args:
     mysql username
     mysql password
