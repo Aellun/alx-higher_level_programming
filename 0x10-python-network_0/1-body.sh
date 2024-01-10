7b71e4752351bd0ece15391656cc81342bba84a0
Aellun <okello.k.otieono@gmail.com>
2024-01-10 15:24:13 -0800

    updatd

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-# only display a body of code 200
-
 curl -sL "$1"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-# variable email = test@gmail.com
-# variable subject = I will always be here for PLD
-
 curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
